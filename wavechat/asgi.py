4cff6bdde395d96e6ad75b2719ba4f7ea1f09c47
yawasante-dev <yawasantedev@gmail.com>
2026-09-07 23:32:59 +0000

    Add WebSocket consumer - real-time chat working

--- wavechat/asgi.py
+++ wavechat/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wavechat.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wavechat.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
